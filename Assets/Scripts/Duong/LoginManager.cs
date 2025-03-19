b92bef336428b53e1a6788a803ba8f2b48271f42
TDHanie <luongngocthuyduongvt@gmail.com>
2025-03-19 07:54:47 +0700

    Thêm màn hình Login
    - Chuyển vài Prefab của màn 4 vào Folder Prefab Dương
    - Làm màn hình LoginScene, code là LoginManager trong Script Dương
    - Điều chỉnh thêm BackgroundLogin, Icon User và Password

---
+++ Assets/Scripts/Duong/LoginManager.cs
@@ -0,0 +1,31 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoginManager : MonoBehaviour
+{
+	public TMP_InputField usernameInput;
+	public TMP_InputField passwordInput;
+	public TextMeshProUGUI messageText;
+
+	public void OnLoginButton()
+	{
+		string username = usernameInput.text;
+		string password = passwordInput.text;
+
+		//Check đăng nhập đơn giản
+		if (username == "user" && password == "123")
+		{
+			messageText.color = Color.green;
+			messageText.text = "Login Successful!";
+
+			//Chuyển sang Scene chính
+			SceneManager.LoadScene("Poison-Swamp");
+		}
+		else
+		{
+			messageText.color = Color.red;
+			messageText.text = "Invalid Username or Password!";
+		}
+	}
+}
