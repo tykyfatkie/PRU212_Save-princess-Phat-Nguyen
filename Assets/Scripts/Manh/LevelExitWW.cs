5ca1fe06a08e44084e0dbf1c239220cadc3069ae
ace2358 <manhntse184228@fpt.edu.vn>
2025-03-26 15:28:29 +0700

    Update Connection between Scenes

---
+++ Assets/Scripts/Manh/LevelExitWW.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExitWW : MonoBehaviour
+{
+    [SerializeField] float levelLoadDelay = 0.2f;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        var player = FindAnyObjectByType<Player>();
+        if (other.tag == "Player" && player.isAlive)
+        {
+            Debug.Log("Azavinha chiwawa");
+            //FindObjectOfType<LevelTimer>()?.StopTimer();
+            StartCoroutine(LoadNextLevel());
+        }
+    }
+
+    IEnumerator LoadNextLevel()
+    {
+        yield return new WaitForSeconds(levelLoadDelay);
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextScenceIndex = currentSceneIndex + 1;
+
+        if (nextScenceIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextScenceIndex = 0;
+        }
+        SceneManager.LoadScene(nextScenceIndex);
+
+        if (currentSceneIndex == SceneManager.sceneCountInBuildSettings - 1)
+        {
+            FindObjectOfType<LevelTimer>()?.StopTimer();
+        }
+    }
+}
+
